a4fe646819b066135e0d6476f1da2d072015eeb2
Gaurav Kumar <gkumar318raja@gmail.com>
2022-04-10 17:34:32 +0530

    reinitialized repo

---
+++ src/spotify.js
@@ -0,0 +1,39 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const clientId = "c3f5b194fda6419084eaba59f0f0a696";
+const redirectUri = "https://spotify-clone-gaurav.vercel.app/";
+const scopes = [
+  "user-read-playback-position",
+  "user-read-email",
+  "user-library-modify",
+  "playlist-modify-public",
+  "ugc-image-upload",
+  "user-follow-modify",
+  "user-modify-playback-state",
+  "user-read-recently-played",
+  "user-read-private",
+  "user-library-read",
+  "user-top-read",
+  "playlist-modify-private",
+  "user-follow-read",
+  "user-read-playback-state",
+  "user-read-currently-playing",
+  "playlist-read-private",
+  "playlist-read-collaborative", ,
+];
+
+export const getTokenFromResponse = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
